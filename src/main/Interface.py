85db7b805e2d9feeee4839bc9538cfc8913afb4b
“ishasharmax” <1999sharma19@gmail.com>
2021-04-16 19:59:58 +0530

    interface

--- src/main/Interface.py
+++ src/main/Interface.py
@@ -5,7 +5,7 @@ def main():
     print("---------------------------------------")
 
     print("What do you want to do?")
-    print("1. Create Chord Progression\n2. Quit")
+    print("1. Create Chord Progression\n2. Choose Pre-made Chord Progression\n3. Random Chord Progression\n4. Quit")
     choiceToDo = int(input())
 
     while choiceToDo==1:
@@ -20,7 +20,25 @@ def main():
             print("Ab Major\nA Major\nBb Major\nB Major\nC Major\nDb Major\nD Major\nEb Major\nE Major\nF Major\nF# Major\nG Major")
             # createMajorScale()
         # else:
-        #     createRandomScale()
+        #     getRandomScale()               #random search
+
+
+    while choiceToDo==2:
+        print("What type of chord progression you want to choose from? (choose #)")
+        print("1. Minor\n2. Major\n3. Random")
+        choice = int(input())
+
+        if choice==1:
+            print("Minor scale chosen")
+            # chooseMinorScale()
+        elif choice==2:
+            print("Major scale chosen")
+            # chooseMajorScale()
+        # else:
+            # chooseRandomScale()               #random search
+
+    # while  choiceToDo==3:
+        #getRandomScale()               #random search
 
 if __name__ == "__main__":
     main()
