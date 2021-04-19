23c06f5d7124e6f75f887f5d9cb006c987917598
“ishasharmax” <1999sharma19@gmail.com>
2021-04-20 02:26:38 +0530

    Interface

---
+++ src/main/Interface.py
@@ -0,0 +1,32 @@
+
+# Creates a new MIDI file in a minor chord progression/notes after user chooses any listed minor chord
+# @param minor chord chosen by the user
+# The returned string will be written in a MIDI file once returned.
+def createMinorScale(minorChord):
+    return None
+
+# Creates a new MIDI file in a major chord progression/notes after user chooses any listed major chord
+# @param major chord chosen by the user
+# The returned string will be written in a MIDI file once returned.
+def createMajorScale(majorChord):
+    return None
+
+# Creates a new MIDI file in a random chord progression/notes after user chooses random progression
+# The returned string will be written in a MIDI file once returned.
+def getRandomScale():
+    return None
+
+# Provides an already made major scale text file/string which can me edited or iterated over
+# The returned major scale text file/string can be edited
+def chooseMajorScale():
+    return None
+
+# Provides an already made minor scale text file/string which can me edited or iterated over
+# The returned minor scale text file/string can be edited
+def chooseMinorScale():
+    return None
+
+# Provides an already made random scale text file/string which can me edited or iterated over
+# The returned random scale text file/string can be edited
+def chooseRandomScale() :
+    return None
