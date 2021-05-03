c777f102e3804c9036fb535e75b32af8355ad3e3
Bobolicious cool kid <bbradley@ithaca.edu>
2021-05-03 10:49:18 -0400

    test files set up

--- src/test/EnvironmentTest.py
+++ src/test/EnvironmentTest.py
@@ -5,9 +5,11 @@ import unittest
 class TestEnvironmentMethods(unittest.TestCase):
     def test_extractInt(self):
         a = 1
-        b = 1
-        message = "not equal"
-        self.assertEquals(a,b,message)
+        b = 0
+        c = 1
+        message = "does not equal"
+        self.assertEqual(a,c,message)
+        #self.assertEqual(a,b,message)
     
     def test_convertMidiToText(self):
         return 0
@@ -15,12 +17,6 @@ class TestEnvironmentMethods(unittest.TestCase):
         
     def test_convertTextToMidi(self):
         return 0
-
-
-""" def main():
-    extractIntTest(0)
-    convertMidiToTextTest(0)
-    convertTextToMidiTest(0) """
 
 
 if __name__ == '__main__':
